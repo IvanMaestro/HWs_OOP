7e82b9bdb9baea6bacdc744a2b6fd4f6b8ed7712
Ivan Maestro <masterov_in@maail.ru>
2024-02-21 19:17:54 +0300

    hw 4 done

---
+++ src/main/java/gb_Masterov/hw_04/BinaryTranslation.java
@@ -0,0 +1,18 @@
+package gb_Masterov.hw_04;
+
+public class BinaryTranslation<T> extends Calculator{
+
+    public BinaryTranslation() {
+        super();
+    }
+    public String toBinary(T number) {
+        Integer num = Integer.parseInt(number.toString());
+        if (num<=0) return null;
+        return Integer.toBinaryString(num);
+    }
+    public Integer fromBinary(T number) {
+        String str = number.toString();
+        return  Integer.parseInt(str, 2);
+    }
+
+}
